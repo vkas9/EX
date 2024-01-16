22865a5ed543d4b256a08bb63e4ef565874f4f26
Vikas <81951052+vkas9@users.noreply.github.com>
2024-01-16 14:34:44 +0000

    sdf

--- server.js
+++ server.js
@@ -1,12 +1,7 @@
 const express=require("express");
 const app=express();
 
-app.use((req,res,next)=>{ // middleware
-    console.log("middleware working");
-    next();
-    
-    
-});
+app.set("view engine","ejs");
 app.get("/",(req,res)=>{
     res.send("Hello Jee, kaise ho saare");
 });
