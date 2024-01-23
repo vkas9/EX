2ee861ac240fe215d9cf85039d050edb595d897c
Vikas <81951052+vkas9@users.noreply.github.com>
2024-01-23 19:56:00 +0000

    mn

--- server.js
+++ server.js
@@ -19,13 +19,25 @@ app.use(function errorHandler(err,req,res,next){
     res.status(500);
     res.render('error',{error:err});
 })
+const bodyParser=require("body-parser");
+app.use(bodyParser.json());
 app.post("/api/cars",(req,res)=>{
     const{name,brand}=req.body;
     console.log(name);
     console.log(brand);
     res.send("Car Submitted Successfully");
-
 })
 app.listen(8000,()=>{
     console.log("Server Started at port no. 8000");
 });
+
+const mongoose=require("mongoose");
+mongoose.connect("mongodb://localhost:27017/myDatabase",{
+    
+})
+.then(()=>{
+    console.log("Connection Successful")
+})
+.catch(()=>{
+    console.log("Received an error!!")
+})
